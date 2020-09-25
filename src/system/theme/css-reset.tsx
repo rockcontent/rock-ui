90fe352c61361b5779c133177e7780f7e1236199
Gabriel Maracaípe <gabriel.maracaipe@rockcontent.com>
2020-09-25 14:29:01 -0300

    adds rock-ui default font to css-reset

--- src/system/theme/css-reset.tsx
+++ src/system/theme/css-reset.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { CSSReset as BaseCSSReset, Global } from '@chakra-ui/core';
 import { COLORS } from '../../constants/colors/colors';
+import { RockUITheme } from './theme';
 
 export const CSSReset = () => (
   <>
     <Global
       styles={`
+                @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;400;600&family=Inter:wght@200;400;600&display=swap');
+
                 * {
                     border-color: ${COLORS.GRAY_200};
+                }
+                
+                body {
+                    font-family: ${RockUITheme.fonts.body};
                 }
             `}
     ></Global>
