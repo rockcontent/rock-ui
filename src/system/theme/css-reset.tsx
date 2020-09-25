60879a0174af6fdd6c03a09e61fb4f3fb59f2154
Gabriel Maracaípe <gabriel.maracaipe@rockcontent.com>
2020-09-25 12:58:35 -0300

    Change stories base to rock-ui & fix default border color rule

--- src/system/theme/css-reset.tsx
+++ src/system/theme/css-reset.tsx
@@ -1,4 +1,16 @@
 import React from 'react';
-import { CSSReset as BaseCSSReset } from '@chakra-ui/core';
+import { CSSReset as BaseCSSReset, Global } from '@chakra-ui/core';
+import { COLORS } from '../../constants/colors/colors';
 
-export const CSSReset = () => <BaseCSSReset />;
+export const CSSReset = () => (
+  <>
+    <Global
+      styles={`
+                * {
+                    border-color: ${COLORS.GRAY_200};
+                }
+            `}
+    ></Global>
+    <BaseCSSReset />
+  </>
+);
