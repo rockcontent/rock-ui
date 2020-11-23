7e96d531f7f9c8141a40c4e44185afef46eb94a2
Lucas Medeiros <lucas.medeiros@rockcontent.com>
2020-11-23 12:42:41 -0300

    Export tab component

---
+++ src/components/tabs/index.ts
@@ -0,0 +1,5 @@
+export { Tab, TabProps } from './Tab';
+export { Tabs, TabsProps } from './Tabs';
+export { TabList, TabListProps } from './TabList';
+export { TabPanel, TabPanelProps } from './TabPanel';
+export { TabPanels, TabPanelsProps } from './TabPanels';
