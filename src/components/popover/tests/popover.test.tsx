6cb2fb5de1e0f3b8332460610902a394c75f26cc
matheus.benites <matheus.benites@rockcontent.com>
2021-02-08 18:41:20 -0300

    adjustments in popover component

--- src/components/popover/tests/popover.test.tsx
+++ src/components/popover/tests/popover.test.tsx
@@ -12,44 +12,20 @@ import {
   PopoverCloseButton,
 } from '../';
 
-describe('Popover', () => {
-  test('Popover renders correctly', () => {
-    const { asFragment } = render(
-      <Popover placement="right-start">
-        <PopoverTrigger>
-          <button>Trigger</button>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverCloseButton />
-          <PopoverHeader>Confirmation!</PopoverHeader>
-          <PopoverBody>
-            Are you sure you want to have that milkshake?
-          </PopoverBody>
-        </PopoverContent>
-        <PopoverFooter>This is the footer</PopoverFooter>
-      </Popover>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  test('Popover renders correctly', () => {
-    const { asFragment } = render(
-      <Popover placement="right-start">
-        <PopoverTrigger>
-          <button>Trigger</button>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverCloseButton />
-          <PopoverHeader>Confirmation!</PopoverHeader>
-          <PopoverBody>
-            Are you sure you want to have that milkshake?
-          </PopoverBody>
-        </PopoverContent>
-        <PopoverFooter>This is the footer</PopoverFooter>
-      </Popover>
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
+test('Popover renders correctly', () => {
+  const { asFragment } = render(
+    <Popover placement="right-start">
+      <PopoverTrigger>
+        <button>Trigger</button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <PopoverHeader>Confirmation!</PopoverHeader>
+        <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+      </PopoverContent>
+      <PopoverFooter>This is the footer</PopoverFooter>
+    </Popover>
+  );
+  expect(asFragment()).toMatchSnapshot();
 });
