f78595e308985ec5415ffae85a14bc768059bd37
fernanda.canto <fernanda.canto@rockcontent.com>
2021-03-03 10:26:01 -0300

    Add story description in clear input datepicker.

--- src/components/datepicker/stories/datepicker.stories.tsx
+++ src/components/datepicker/stories/datepicker.stories.tsx
@@ -4,6 +4,9 @@ import { Datepicker } from '../datepicker';
 
 export default {
   title: 'Datepicker',
+  parameters: {
+    componentSubtitle: 'A simple and reusable datepicker component for React.',
+  },
   decorators: [
     (Story: any) => (
       <Container mt="40px">
@@ -20,7 +23,29 @@ export const Basic = () => {
       onChange={setStartDate}
       selected={startDate}
       placeholderText="Start Date"
-      isClearable
     />
   );
+};
+
+export const Clear = () => {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  return (
+    <>
+      <Datepicker
+        onChange={setStartDate}
+        selected={startDate}
+        placeholderText="I have been cleared!"
+        isClearable
+      />
+    </>
+  );
+};
+
+Clear.story = {
+  parameters: {
+    docs: {
+      storyDescription:
+        'Add the attribute "isClearable" so that the clear button appears in the input.',
+    },
+  },
 };
