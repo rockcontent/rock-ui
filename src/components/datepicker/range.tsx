066bce6342875adb84a06f8df0ffee084d2045d5
Kelvin Chaves <kelvinhenrique.cs@gmail.com>
2021-03-25 09:50:31 -0300

    Remove unused useEffect

--- src/components/datepicker/range.tsx
+++ src/components/datepicker/range.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { StyledBaseRangePicker } from './styled';
 import { DayPickerRangeController } from 'react-dates';
