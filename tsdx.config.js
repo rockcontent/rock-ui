2671754715b5ae3d09d25512ac5e28b176dd5d18
matheus.benites <matheus.benites@rockcontent.com>
2021-03-31 10:47:42 -0300

    add config in rollup file config for load svg file

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,11 +1,18 @@
 const postcss = require('rollup-plugin-postcss');
+const svg = require('rollup-plugin-svg');
+
 module.exports = {
-	rollup(config, options) {
-		config.plugins.push(
-			postcss({
-				modules: true,
-			})
-		);
-		return config;
-	},
+  rollup(config, options) {
+    const external = config.external;
+    config.external = id => (id.match(/.svg$/) ? false : external(id));
+    config.plugins.push(
+      postcss({
+        modules: true,
+      }),
+      svg({
+        base64: false,
+      })
+    );
+    return config;
+  },
 };
