d661a34ca636c79232f15d5db9063644f0c586cf
Matheus Benites <matheus.benites@rockcontent.com>
2021-06-21 13:43:42 -0300

    test: finished test for live region

---
+++ src/components/live-region/tests/live-region.test.tsx
@@ -0,0 +1,61 @@
+import { render } from '@testing-library/react';
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { LiveRegion, LiveRegionOptions, useLiveRegion } from '../index';
+
+test('LiveRegion creates a container and has the proper aria and role attributes', () => {
+  render(<div />);
+
+  new LiveRegion();
+  const region = document.getElementById('chakra-a11y-live-region');
+
+  expect(region).toHaveAttribute('aria-atomic', 'true');
+  expect(region).toHaveAttribute('aria-live', 'polite');
+  expect(region).toHaveAttribute('aria-relevant', 'all');
+  expect(region).toHaveAttribute('role', 'status');
+});
+
+test('LiveRegion creates a container using the provided options', () => {
+  render(<div />);
+
+  const options: LiveRegionOptions = {
+    id: 'some-id',
+    role: 'alert',
+    'aria-live': 'assertive',
+    'aria-relevant': 'removals',
+    'aria-atomic': 'false',
+  };
+  new LiveRegion(options);
+  const region = document.getElementById('some-id');
+
+  expect(region).toHaveAttribute('aria-atomic', 'false');
+  expect(region).toHaveAttribute('aria-live', 'assertive');
+  expect(region).toHaveAttribute('aria-relevant', 'removals');
+  expect(region).toHaveAttribute('role', 'alert');
+});
+
+test('LiveRegion can "speak" by setting its text content', () => {
+  render(<div />);
+
+  const liveRegion = new LiveRegion();
+  const region = document.getElementById(
+    'chakra-a11y-live-region'
+  ) as HTMLElement;
+
+  liveRegion.speak('Hello World');
+  expect(region.innerText).toEqual('Hello World');
+});
+
+test('LiveRegion creates a container and has the proper aria and role attributes', () => {
+  render(<div />);
+
+  const { result } = renderHook(() => useLiveRegion());
+  const region = document.getElementById(
+    'chakra-a11y-live-region'
+  ) as HTMLElement;
+
+  act(() => result.current.speak('Hello World'));
+
+  expect(region.innerText).toEqual('Hello World');
+});
