a71baf87c0af8717cba83af8d578cb5202b7c9f2
Matheus Benites <matheus.benites@rockcontent.com>
2021-06-22 15:15:51 -0300

    feat: add Descendant

---
+++ src/components/descendants/descendant.ts
@@ -0,0 +1,9 @@
+import {
+  Descendant as BaseDescendant,
+  DescendantOptions as BaseDescendantOptions,
+  DescendantsManager as BaseDescendantManager,
+} from '@chakra-ui/descendant';
+
+export { BaseDescendant as Descendant };
+export { BaseDescendantOptions as DescendantOptions };
+export { BaseDescendantManager as DescendantsManager };
